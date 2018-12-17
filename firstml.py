61af0e48bf0619a44ff7236f15a5ea438a0d16eb
vahid-saffarian <vahid.saffarian@gmail.com>
2018-12-17 21:39:55 +0330

    Update firstml.py

--- firstml.py
+++ firstml.py
@@ -1,14 +1,15 @@
+#this script uses ml to predict gender based on a given data about height, weight, and shoe size
+
 from sklearn import tree
 from sklearn.naive_bayes import MultinomialNB
 #[height, weight, shoe size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [186, 86, 44], [165, 45, 37], [170, 59, 40], [187, 81, 45], [187, 100, 43]]
 Y = ['male', 'female', 'female', 'male', 'female', 'female', 'male', 'male']
 
-#clf=tree.DecisionTreeClassifier()
-#clf=clf.fit(X, Y)
-#prediction = clf.predict([[175, 100, 45]])
-
+#using multinomial Naive bayes method
 clf = MultinomialNB()
 clf = clf.fit(X, Y)
+
+#new data to be predicted
 prediction = clf.predict([[175, 100, 45]])
 print('predicted gender is ', prediction)
